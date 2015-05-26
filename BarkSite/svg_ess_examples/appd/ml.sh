f8b78222a8eaf50e614a7e4429933f9791817239
zwgirl <820726977@qq.com>
2015-05-26 19:53:00 +0800

    拓尔思演示

--- BarkSite/svg_ess_examples/appd/ml.sh
+++
@@ -1,2 +0,0 @@
-java -cp /usr/local/xmljar/xerces.jar:.  MLtoSVG ${1}.ml > ${1}.svg
-
--- BarkSite/svg_ess_examples/ch13/omf_j/WEB-INF/classes/compile_transform.sh
+++
@@ -1,7 +0,0 @@
-javac -classpath \
-.:$JAVA_HOME/lib/tools.jar:\
-$JAVA_HOME/jre/lib/rt.jar:\
-./servlet-2.2.0.jar:\
-/usr/local/xmljar/xalan.jar:/usr/local/xmljar/xerces.jar:\
-/usr/local/xmljar/fop.jar \
-Transform.java
